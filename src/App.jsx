00e8060670f968e1cf577b72803e096ca65cb5d1
saqlain1020 <saqlainprinters@gmail.com>
2024-11-23 01:05:47 +0500

    INIT

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./style/flexboxgrid.min.css";
+import "./style/index.css";
+import Landing from "./screens/Landing";
+
+function App() {
+  return (
+    <>
+      <Landing />
+    </>
+  );
+}
+
+export default App;
+
